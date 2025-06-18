15c6ea518dcf1b6e4732f7f315d1eee9e6bee4a9
Philippe Assis <codephilippe@gmail.com>
2025-06-18 09:43:28 -0300

    fix: corrigir a lógica de renderização do componente Toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { useDiscoveryStore } from "../store/discoveryStore";
 import { useSpacesStore } from "../store/spacesStore";
